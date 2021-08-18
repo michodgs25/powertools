c4813321dc7fea972984d7fe267c41f8ef99c1d7
michodgs25 <michodgs@gmail.com>
2021-08-18 15:09:50 +0000

    Make product categories migrations

--- store/models.py
+++ store/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
@@ -19,3 +20,16 @@ class Product(models.Model):
     company = models.CharField(max_length=20, default='admin')
     tool_description = models.TextField(blank=True)
     image = models.ImageField(upload_to='images/')
+    slug = models.SlugField(max_length=20)
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+    in_stock = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'Products'
+        ordering = ('-created',)
+
+    def __str__(self):
+        return self.title
