3645b56c468fa72adf350da435e2da140601060a
michodgs25 <michodgs@gmail.com>
2021-08-18 20:25:49 +0000

    Create test models entry

---
+++ store/tests/test_models.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+
+from store.models import Category, Product
+
+
+class TestCategoriesModel(TestCase):
+
+    def setUp(self):
+        self.data1 = Category.objects.create(name='django', slug='django')
+
+    def test_category_model_entry(self):
+        """
+        Test category model data insertion/types/field attributes
+        """
+        data = self.data1
+        self.assertTrue(isinstance(data, Category))
