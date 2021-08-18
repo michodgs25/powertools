80da6d6c470f9b45534cdc7b45899b8753824f66
michodgs25 <michodgs@gmail.com>
2021-08-18 19:33:07 +0000

    Add first product to store

--- store/admin.py
+++ store/admin.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 
 from .models import Category, Product
 
+
 @admin.register(Category)
-class CategoryAdmin(Admin.ModelAdmin):
+class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug']
     prepopulated_fields = {'slug': ('name',)}
+
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
