f4d77b0b3dad74d1df83fca84f6bb661a1b46f9f
michodgs25 <michodgs@gmail.com>
2021-08-19 10:36:42 +0000

    Add additional test models

--- store/tests/test_models.py
+++ store/tests/test_models.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-
+from django.contrib.auth.models import User
 from store.models import Category, Product
 
 
@@ -14,3 +14,25 @@ class TestCategoriesModel(TestCase):
         """
         data = self.data1
         self.assertTrue(isinstance(data, Category))
+
+    def test_category_model_entry(self):
+        """
+        Test Category model default name
+        """
+        data = self.data1
+        self.assertEqual(str(data), 'django')
+
+
+class TestProductsModel(TestCase):
+    def setUp(self):
+        Category.objects.create(name='django', slug='django')
+        User.objects.create(username='admin')
+        self.data1 = Product.objects.create(category_id=1, title='django beginners', created_by_id=1,
+                                             slug='django-beginners', price='16.16', image='django')
+
+    def test_products_model_entry(self):
+        """
+        """
+        data = self.data1
+        self.assertTrue(isinstance(data, Product))
+        self.assertEqual(str(data), 'django beginners')
