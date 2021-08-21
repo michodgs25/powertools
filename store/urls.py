83078600ea574554ce16f417cf65d5b234666249
michodgs25 <michodgs@gmail.com>
2021-08-21 13:05:07 +0000

    Create product detail view& html template

--- store/urls.py
+++ store/urls.py
@@ -6,4 +6,5 @@ app_name = 'store'
 
 urlpatterns = [
     path('', views.product_all, name='product_all'),
+    path('item/<slug:slug>/', views.product_detail, name='product_detail'),
 ]
--- store/views.py
+++ store/views.py
@@ -12,3 +12,8 @@ def categories(request):
 def product_all(request):
     products = Product.objects.all()
     return render(request, 'store/home.html', {'products': products})
+
+
+def product_detail(request, slug):
+    product = get_object_or_404(Product, slug=slug, in_stock=True)
+    return render(request, 'store/products/detail.html', {'product': product})
