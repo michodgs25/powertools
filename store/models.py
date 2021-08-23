a4c704d7e0e3bf0595e2f64cb2c5cf06b39a2431
michodgs25 <michodgs@gmail.com>
2021-08-23 13:43:49 +0000

    Create category html template

--- store/models.py
+++ store/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -30,6 +31,10 @@ class Product(models.Model):
     class Meta:
         verbose_name_plural = 'Products'
         ordering = ('-created',)
+
+    def get_absolute_url(self):
+        return reverse('store:product_detail', args=[self.slug])
+
 
     def __str__(self):
         return self.title
--- store/urls.py
+++ store/urls.py
@@ -7,4 +7,5 @@ app_name = 'store'
 urlpatterns = [
     path('', views.product_all, name='product_all'),
     path('item/<slug:slug>/', views.product_detail, name='product_detail'),
+    path('search/<slug:category_slug>/', views.category_list, name='category_list'),
 ]
