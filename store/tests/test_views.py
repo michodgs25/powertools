ebab0f9a7faec05cef7e4afb59ee27b63b803df6
michodgs25 <michodgs@gmail.com>
2021-08-27 09:58:29 +0000

    Sort files using isort package

--- store/tests/test_views.py
+++ store/tests/test_views.py
@@ -1,12 +1,12 @@
 # from unittest import skip
 
-from django.http import HttpRequest
 from django.contrib.auth.models import User
-from django.urls import reverse
+from django.http import HttpRequest
 from django.test import Client, RequestFactory, TestCase
+from django.urls import reverse
 
-from store.views import all_products
 from store.models import Category, Product
+from store.views import all_products
 
 # @skip("demonstrating skipping")
 # class TestSkip(TestCase):
