a7c5a9387ab5404834fba5e3d0899faf2361a6b1
michodgs25 <michodgs@gmail.com>
2021-09-02 09:45:30 +0000

    Rename product templates

--- store/views.py
+++ store/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import get_object_or_404, render
 from .models import Category, Product
 
 
-def all_products(request):
+def product_all(request):
     products = Product.products.all()
     return render(request, 'store/home.html', {'products': products})
 
@@ -17,4 +17,4 @@ def category_list(request, category_slug=None):
 
 def product_detail(request, slug):
     product = get_object_or_404(Product, slug=slug, in_stock=True)
-    return render(request, 'store/products/detail.html', {'product': product})
+    return render(request, 'store/products/single.html', {'product': product})
