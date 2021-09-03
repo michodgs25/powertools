ec6fd43de97738cba3af10b491bb5ba8d94a46c9
michodgs25 <michodgs@gmail.com>
2021-09-03 15:24:19 +0000

    Create cart url, view and summary template

--- tools/urls.py
+++ tools/urls.py
@@ -21,6 +21,7 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls', namespace='store'))
+    path('cart/', include('cart.urls', namespace='cart'))
 ]
 
 if settings.DEBUG:
