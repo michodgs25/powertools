a805f4eaa3be5c8be92f5897c043866249fd73db
michodgs25 <michodgs@gmail.com>
2021-09-06 16:18:42 +0000

    Add cart context processor to settings

--- tools/settings.py
+++ tools/settings.py
@@ -51,6 +51,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'store.context_processors.categories',
+                'cart.context_processors.cart',
             ],
         },
     },
